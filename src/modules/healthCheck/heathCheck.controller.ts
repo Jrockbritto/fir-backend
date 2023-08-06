d6a8ccf8445564ac067d5b5bcdaf9e2448f47294
Joaorock <jrockbritto@gmail.com>
2023-08-06 00:51:23 -0300

    changing hc route

--- src/modules/healthCheck/heathCheck.controller.ts
+++ src/modules/healthCheck/heathCheck.controller.ts
@@ -3,7 +3,7 @@ import { ApiTags } from '@nestjs/swagger';
 
 import { HEALTH_CHECK } from '@config/constants/tags.constants';
 
-@Controller()
+@Controller('health')
 @ApiTags(HEALTH_CHECK)
 export class HealthCheckController {
   @Get()
