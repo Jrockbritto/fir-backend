0f4f6d60e9049a829e5d4434db1fe5396d0eeec0
Jrockbritto <jrockbritto@gmail.com>
2023-08-09 08:51:11 -0300

    chore: adding return type documentation

--- src/modules/times/contexts/createTime/createTime.controller.ts
+++ src/modules/times/contexts/createTime/createTime.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
 import { plainToInstance } from 'class-transformer';
 import { Request } from 'express';
 
@@ -7,6 +7,7 @@ import { TIME } from '@config/constants/tags.constants';
 
 import { JwtAuthGuard } from '@modules/authentication/guards/jwtAuth.guard';
 import { CreateTimeRequestDTO } from '@modules/times/dto/createTime.dto';
+import { CreateTimeResponseDTO } from '@modules/times/dto/createTimeReponse.dto';
 import { Time } from '@modules/times/entity/Time.entity';
 import { User } from '@modules/users/entity/User.entity';
 
@@ -17,6 +18,7 @@ import { CreateTimeService } from './createTime.service';
 @ApiTags(TIME)
 export class CreateTimeController {
   constructor(private readonly createTimeService: CreateTimeService) {}
+  @ApiCreatedResponse({ type: CreateTimeResponseDTO })
   @Post()
   @UseGuards(JwtAuthGuard)
   async handler(@Body() dto: CreateTimeRequestDTO, @Req() req: Request) {
